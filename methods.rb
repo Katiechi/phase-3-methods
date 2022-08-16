e7b2423af5b0c6c327c078f88ce3ae13fa0a9fcc
brian ambundo <brian.katiechi9@gmail.com>
2022-08-16 11:54:00 +0300

    commit tree f8835f6debce59585b45f52cd1a4a962c500387f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+puts greet_programmer
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+
+def add (num1,num2)
+    num1+num2
+end
+puts add(3,4)
+
+def halve(number)
+    if number.is_a?(Numeric) != true
+      return nil
+    end
+
+    number/2
+end
+puts halve(6)
